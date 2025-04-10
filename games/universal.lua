6ff10e5ec715e0f50eff014a571fcf3f51a11bc4
Monia <102583640+NTDCore@users.noreply.github.com>
2025-04-10 12:39:04 +0700

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -592,7 +592,7 @@ run(function()
 			local commit = subbed:find('currentOid')
 			commit = commit and subbed:sub(commit + 13, commit + 52) or nil
 			commit = commit and #commit == 40 and commit or 'main'
-			whitelist.textdata = game:HttpGet('https://raw.githubusercontent.com/NTDCore/vapewhitelist/s'..commit..'/PlayerWhitelist.json', true)
+			whitelist.textdata = game:HttpGet('https://raw.githubusercontent.com/NTDCore/vapewhitelists/'..commit..'/PlayerWhitelist.json', true)
 		end)
 		if not suc or not hash or not whitelist.get then return true end
 		whitelist.loaded = true
